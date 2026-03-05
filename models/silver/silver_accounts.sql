212e5e13ec3ce31657b7631ab54d67e6f2328924
Abhilesh <abhilesh20032003@gmail.com>
2026-03-05 22:59:43 +0530

    Add silver accounts table

---
+++ models/silver/silver_accounts.sql
@@ -0,0 +1,30 @@
+SELECT
+    CAST(account_id AS VARCHAR(255)) AS account_id,
+    CAST(customer_id AS VARCHAR(255)) AS customer_id,
+
+    CASE
+        WHEN UPPER(account_type) IN ('FD','F') THEN 'FD'
+        WHEN UPPER(account_type) IN ('SAV','S','SAVING','SAVINGS') THEN 'SAVINGS'
+        WHEN UPPER(account_type) IN ('CUR', 'C', 'CURR', 'CURRENT') then 'CURRENT'
+        WHEN UPPER(account_type) IN ('LOAN', 'L') THEN 'LOAN'
+        ELSE 'OTHER'
+    END AS account_type,
+
+    COALESCE(
+    TRY_TO_DATE(open_date, 'YYYY-MM-DD'),
+    TRY_TO_DATE(open_date, 'DD-MM-YYYY'),
+    TRY_TO_DATE(open_date, 'YYYY/MM/DD'),
+    TRY_TO_DATE(open_date, 'DD/MM/YYYY'),
+    TRY_TO_TIMESTAMP(open_date, 'YYYY-MM-DD HH24:MI:SS')
+    ) AS open_date,
+
+    CASE
+        WHEN LOWER(status) IN ('active','ac','a') THEN 'ACTIVE'
+        WHEN LOWER(status) IN ('closed','cl','clos') THEN 'CLOSED'
+        WHEN LOWER(status) IN ('inactive','inac','in') THEN 'INACTIVE'
+        ELSE 'UNKNOWN'
+    END AS status,
+
+    COALESCE(branch_code, 'OTHER') AS branch_code
+
+from {{ source('banking','raw_accounts') }}
